d50676879af8db882ab7c28e206cf8ccdd8552ae
Vaal <valscatty@txstate.edu>
2019-04-30 13:35:40 -0500

    Does what it advertises.

--- List.cpp
+++ List.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "List.h"
 
 void List::Append(Node node)
@@ -22,22 +21,22 @@ void List::Append(Node node)
 
 void List::Insert(Node node)
 {
-	Node* pNewNode;
-	Node* pNodes;
+	Node* pNewNode = nullptr;
+	Node* pNodes = nullptr;
+	Node* pPrevious = nullptr;
 
 	pNewNode = new Node(node);
 
-	if (List::isEmpty()) {
+	if (isEmpty()) {
 		pTop = pNewNode;
-		pNewNode = nullptr;
+		pNewNode->pNext = nullptr;
 	}
 	else {
 		pNodes = pTop;
-		Node* pPrevious = nullptr;
 
 		// Will iterate up pNodes until it finds a number greater in alphabetical order
 		// Saves position where it found it, and takes that new position
-		while (pNodes != nullptr && (pNodes->GetName().compare(pNewNode->GetName())) >= 0) {
+		while (pNodes != nullptr && (pNodes->GetName().compare(pNewNode->GetName())) <= 0) {
 			pPrevious = pNodes;
 			pNodes = pNodes->pNext;
 		}
@@ -50,8 +49,54 @@ void List::Insert(Node node)
 		// Makes previous point to new node and the new node to whatever previous was pointing before
 		else {
 			pPrevious->pNext = pNewNode;
-			pNewNode->pNext = pNewNode;
+			pNewNode->pNext = pNodes;
 		}
+	}
+}
+
+void List::Delete(std::string name)
+{
+	Node* pNodes = pTop;
+	Node* pPrevious = nullptr;
+
+	while (pNodes != nullptr && pNodes->GetName() != name) {
+		pPrevious = pNodes;
+		pNodes = pNodes->pNext;
+	}
+
+	pPrevious->pNext = pNodes->pNext;
+	pNodes = nullptr;
+}
+
+void List::Print()
+{
+	Node* pNodes;
+	pNodes = pTop;
+	while (pNodes != nullptr) {
+		pNodes->Print();
+		pNodes = pNodes->pNext;
+	}
+}
+
+void List::Print(std::string name)
+{
+	Node* pNewNode = nullptr;
+	Node* pNodes = nullptr;
+
+	if (List::isEmpty()) {
+		std::cout << "List is empty.\n";
+	}
+	else {
+		pNodes = pTop;
+		Node* pPrevious = nullptr;
+
+		// Will iterate up pNodes until it finds a number greater in alphabetical order
+		// Saves position where it found it, and takes that new position
+		while (pNodes != nullptr && (pNodes->GetName().compare(pNewNode->GetName())) == 0) {
+			pPrevious = pNodes;
+			pNodes = pNodes->pNext;
+		}
+		pNodes->Print();
 	}
 }
 
@@ -63,4 +108,13 @@ bool List::isEmpty()
 std::string List::Node::GetName() const
 {
 	return name;
+}
+
+void List::Node::Print()
+{
+	std::cout << "Name: " << name
+		<< "\nID: " << ID
+		<< "\nEmail: " << email
+		<< "\nGPA: " << GPA
+		<< "\n\n";
 }
--- List.h
+++ List.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 class List {
 private:
@@ -10,15 +11,23 @@ private:
 		std::string email;
 		float GPA;
 	public:
+		Node(int ID, std::string name, std::string email, float GPA)
+			:
+			ID(ID),
+			name(name),
+			email(email),
+			GPA(GPA)
+		{}
 		Node* pNext = nullptr;
 		std::string GetName() const;
+		void Print();
 	};
 	Node* pTop = nullptr;
 public:
 	void Append(Node node);
 	void Insert(Node node);
-	void Delete(std::string);
+	void Delete(std::string name);
 	void Print();
-	void Print(std::string);
+	void Print(std::string name);
 	bool isEmpty();
 };
