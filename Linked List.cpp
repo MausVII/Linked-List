7e2bf3f526a29054097a9fd1fd1e47e34de39d96
Vaal <valscatty@txstate.edu>
2019-04-30 16:10:38 -0500

    Polished off

--- Linked List.cpp
+++ Linked List.cpp
@@ -2,9 +2,17 @@
 #include <fstream>
 #include "List.h"
 
+struct StudentData {
+	int ID;
+	std::string name;
+	std::string email;
+	float GPA;
+};
+
 int main()
 {
 	List records;
+	StudentData data[15];
 	std::ifstream file("File.txt");
 	if (file) {
 		for (int i = 0; i < 15; ++i) {
@@ -13,11 +21,27 @@ int main()
 			std::string email;
 			float GPA;
 			file >> ID >> name >> email >> GPA;
+
+			data[i].ID = ID;
+			data[i].name = name;
+			data[i].email = email;
+			data[i].GPA = GPA;
+
 			records.Insert({ ID, name, email, GPA });
 		}
 	}
 	file.close();
 
+	std::cout << "Unsorted Array:\n";
+	for (int i = 0; i < 15; ++i) {
+		std::cout << "Name: " << data[i].name
+			<< "\nID: " << data[i].ID
+			<< "\nEmail: " << data[i].email
+			<< "\nGPA: " << data[i].GPA
+			<< std::endl << std::endl;
+	}
+
+	std::cout << "Sorted Linked List:\n";
 	records.Print();
 	while (true);
 	return 0;
